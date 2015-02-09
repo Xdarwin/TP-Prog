5a6d787ecb20e755cea51f95b8ed46aacd600302
Xdarwin <guidrapbiz@hotmail.fr>
2015-02-09 17:51:55 +0100

    Asset Unity

---
+++ BackToBasics/BackToBasics/Pony.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToBasics
+{
+    class Pony
+    {
+        protected int size;
+        protected string pony;
+        protected ConsoleColor color_poney;
+
+
+        public Pony (ConsoleColor couleur)
+        {
+            Random rnd = new Random();
+            this.size = rnd.Next(2, 10);
+            this.color_poney = couleur;
+            this.pony = "";
+        }
+
+
+        public void display()
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+            pony = pony + ".";
+            for(int i = 0; i < size; i++)
+            {
+                pony = pony + "=";
+            }
+            pony = pony + ".°";
+            Console.ForegroundColor = color_poney;
+            Console.Write(pony);
+            Console.ForegroundColor = previous;
+        }
+
+    }
+}
---
+++ BackToBasics/BackToBasics/Shop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToBasics
+{
+    class Shop
+    {
+
+        string nom;
+        List<Pony> list_pony;
+        int argent;
+
+        public Shop(string name, int gold)
+        {
+            this.nom = name;
+            this.argent = gold;
+            list_pony = new List<Pony>();
+        }
+
+
+        public void display()
+        {
+            for(int i = 0; i < list_pony.Count; i++)
+            {
+                list_pony[i].display();
+                Console.Write("|");
+            }
+        }
+
+
+        public void add_gold(int nb)
+        {
+            this.argent = argent + nb;
+            if (argent < 0)
+                argent = 0;
+            Console.WriteLine("Le magasin possede {0} Gold", argent);
+        }
+
+
+        public void add_pony(Pony poney)
+        {
+            list_pony.Add(poney);
+        }
+
+
+
+        public Pony get_pony()
+        {
+            Pony on_te_vire = list_pony[list_pony.Count - 1];
+            list_pony.RemoveAt(list_pony.Count - 1);
+            return on_te_vire;
+        }
+
+    }
+}
