3d11b942fbdaf53da6b3e6ea19fa08fe9df77370
Xdarwin <guidrapbiz@hotmail.fr>
2015-05-11 16:40:21 +0200

    Matrix

---
+++ Matrix/Matrix/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Node<T>
+    {
+        public T element;
+        public Node<T> fils_g;
+        public Node<T> fils_d;
+
+        public Node(T value)
+        {
+            this.element = value;
+            this.fils_g = null;
+            this.fils_d = null;
+        }
+
+    }
+}
